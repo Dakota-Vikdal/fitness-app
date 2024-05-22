6f35c7cb70975dcc1a596d84d24db365d770cb78
Dakota Vikdal <dakota.james19@hotmail.com>
2024-05-22 15:06:57 -0400

    created react and setup blank canvas

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    //utilizing fragments in order to keep the code cleaner and more performant
+    <>
+      <h3>hi</h3>
+    </>
+  );
+}
+
+export default App;
